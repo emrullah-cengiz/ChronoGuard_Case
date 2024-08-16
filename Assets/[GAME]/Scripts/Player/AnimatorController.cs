96df1766c5c3a7163eda9c7ccc2877d965bd2016
emrul <emrul@DESKTOP-26M98JG>
2024-08-17 01:17:38 +0300

    Add project files.

---
+++ Assets/[GAME]/Scripts/Player/AnimatorController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class AnimatorController : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.AI.NavMeshAgent _agent;
+
+}
---
+++ Assets/[GAME]/Scripts/Player/MovementController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private InputActionReference _movementInputActionRef;
+    [SerializeField] private NavMeshAgent _agent;
+
+    [SerializeField] private float _speed;
+    private Vector2 _input;
+
+    private void Update()
+    {
+        if (_movementInputActionRef.action.phase == InputActionPhase.Started)
+        {
+            Move();
+            Rotate();
+        }
+    }
+
+
+    private void Move()
+    {
+        _input = _movementInputActionRef.action.ReadValue<Vector2>();
+
+        Vector3 movementVector = new(_input.x, 0, _input.y);
+
+        _agent.velocity = _speed * movementVector.normalized;
+    }
+
+    private void Rotate()
+    {
+
+    }
+}
